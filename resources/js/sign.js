1c322ebc7d00df5c72445149acd0dcf578c3c4a8
Jepil Choi <myartame@gmail.com>
2018-07-19 17:19:32 +0900

    .

--- resources/js/sign.js
+++ resources/js/sign.js
@@ -3,12 +3,33 @@ $(document).ready(function() {
         $.ajax({
             url         : BASE_URL + 'user/login',
             type        : 'POST',
-            success     : function(data) {
+            data        : {
+                email   : $('input[type="email"]').val(),
+                password: $('input[type="password"]').val(),
+            },
+            success     : function() {
                 alert('Login success');
             },
             error       : function() {
                 alert('Ajax Error');
-            }
+            },
+        });
+    });
+
+    $('#joinBtn').click(function() {
+        $.ajax({
+            url         : BASE_URL + 'user/join',
+            type        : 'POST',
+            data        : {
+                email   : $('input[type="email"]').val(),
+                password: $('input[type="password"]').val(),
+            },
+            success     : function() {
+                alert('Join success');
+            },
+            error       : function() {
+                alert('Ajax Error');
+            },
         });
     });
 });
